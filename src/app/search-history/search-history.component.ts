27caa0d56041f06436fd043b6b7f70535025435c
Som Shrestha <som_shrestha@hotmail.co.uk>
2024-05-12 21:55:07 +0100

    Touch up

--- src/app/search-history/search-history.component.ts
+++ src/app/search-history/search-history.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../services/search.service';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
+import { SearchResultDto } from '../models/search-result-dto.model';
 
 @Component({
   selector: 'app-search-history',
@@ -11,7 +12,8 @@ import { Subscription } from 'rxjs';
   styleUrls: ['../app.component.css']
 })
 export class SearchHistoryComponent implements OnInit {
-  public searchResultHistory: any;
+  public searchResultHistory: SearchResultDto[] = [];
+  public isFilterByDate: boolean = false;
 
   private readonly subscriptions: Subscription[] = [];
 
@@ -19,15 +21,38 @@ export class SearchHistoryComponent implements OnInit {
     private readonly searchService: SearchService
   ) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.getSearchHistory();
   }
 
   public getSearchHistory(): void {
     const searchHistorySubscription = this.searchService.getAllSearchHistory().subscribe((data) => {
       this.searchResultHistory = data;
+      if (this.isFilterByDate) {
+        this.getFilteredData();
+      }
+      this.isFilterByDate = false;
     });
     this.subscriptions.push(searchHistorySubscription);
+  }
+
+  public filterByDate(): void {
+    this.isFilterByDate = true;
+    this.getSearchHistory();
+  }
+
+  public resetData(): void {
+    this.isFilterByDate = false;
+    this.getSearchHistory();
+  }
+
+  private getFilteredData(): void {
+    var datePickerInput = document.getElementById("datepicker") as HTMLInputElement;
+    var selectedDate = datePickerInput.value;
+    if (selectedDate !== "") {
+      var x = this.searchResultHistory.filter(srh => new Date(srh.searchDate).toISOString().split('T')[0] === selectedDate);
+      this.searchResultHistory = [...new Set(x)];
+    }
   }
 
   public ngOnDestroy(): void {
